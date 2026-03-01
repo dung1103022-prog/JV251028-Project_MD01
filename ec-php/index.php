930eef65bc552c8088c32a6279fa33c048fe4366
dung1103022-prog <dung1103022@gmail.com>
2026-03-01 13:46:20 +0900

    Add files via upload

---
+++ ec-php/index.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+define('BASE_PATH', dirname(__DIR__));
+
+require BASE_PATH . '/vendor/autoload.php';
+
+// .env 読み込み
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
+
+// セッション開始
+\App\Core\Session::start();
+
+// リクエスト生成
+$request = new \App\Core\Request();
+
+// アプリ起動
+$app = new \App\Core\App();
+$app->run($request);
